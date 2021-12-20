4d558c1d123757ec628d133ec9080ae1b373d7cf
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2021-12-20 15:28:32 +0100

    hpc2n/c3se: mlperf-0.7: Use SubmissionRun to get Accuracy numbers. Add ref values for performance and accuracy on Alvis. Add outside-container configs, {system_list,main}.py A few other small changes, including running a special container with required packages for ssd-resnet34 accuracy testing.

--- mlperf/mlperf-0.7.py
+++ mlperf/mlperf-0.7.py
@@ -26,43 +26,61 @@ class MLPerfInferenceBase(rfm.RunOnlyRegressionTest):
 
         # Create some temp dirs where it can write stuff
         self.prerun_cmds = [
-            'mkdir -p $TMPDIR/{dali,engines,logs,measurements}',
+            'mkdir -p $TMPDIR/{dali,engines,logs,measurements,result,unet,bert}',
         ]
 
         # This is a container run
         self.container_platform = 'Singularity'
-        self.container_platform.image = '/cephyr/NOBACKUP/priv/c3-alvis/mlperf-inference-lni-latest.sif'
+        #self.container_platform.image = '/cephyr/NOBACKUP/priv/c3-alvis/mlperf-inference-lni-latest.sif'
+        #self.container_platform.image = '/cephyr/users/akesa/Alvis/reframe/test-runs/mlperf-inference-lni-latest-ake.sif'
+        self.container_platform.image = '/cephyr/users/akesa/Alvis/reframe/test-runs/mlperf-inference-lni-latest-tkinter-2.sif'
         self.container_platform.mount_points = [
             ("/cephyr/NOBACKUP/priv/c3-alvis/datasets/mlperf-v0.7-inference/Language Processing/build", "/work/build"),
             ("/cephyr/NOBACKUP/priv/c3-alvis/datasets/mlperf-v0.7-inference/Language Processing", "/dataset"),
             ("/cephyr/NOBACKUP/priv/c3-alvis/datasets/mlperf-v0.7-inference/nvidia/models", "/work/build/models"),
             ("/cephyr/NOBACKUP/priv/c3-alvis/datasets/mlperf-v0.7-inference/nvidia/data", "/work/build/data"),
             ("/cephyr/NOBACKUP/priv/c3-alvis/datasets/mlperf-v0.7-inference/nvidia/preprocessed_data", "/work/build/preprocessed_data"),
+            ("/cephyr/users/akesa/Alvis/reframe/test-runs/mlperf/configs", "/work/configs"),
+            ("/cephyr/users/akesa/Alvis/reframe/test-runs/mlperf/system_list.py", "/work/code/common/system_list.py"),
+            ("/cephyr/users/akesa/Alvis/reframe/test-runs/mlperf/main.py", "/work/code/main.py"),
             ("$TMPDIR/measurements", "/work/measurements"),
             ("$TMPDIR/engines", "/work/build/engines"),
             ("$TMPDIR/logs", "/work/build/logs"),
+            ("$TMPDIR/result", "/work/build/result"),
         ]
         if self.benchmark == 'rnnt':
             self.container_platform.mount_points.append(("$TMPDIR/dali", "/work/build/bin/dali"))
+        if self.benchmark == '3d-unet':
+            self.container_platform.mount_points.append(("$TMPDIR/unet", "/work/build/brats_postprocessed_data"))
+        if self.benchmark == 'bert':
+            self.container_platform.mount_points.append(("$TMPDIR/bert", "/work/build/bert"))
+            # bert takes ~2h
+            self.time_limit = '3h'
+        if self.benchmark == 'resnet50':
+            self.time_limit = '2h'
 
         self.container_platform.options = ['--pwd /work']
         self.container_platform.command = (
-            'make run RUN_ARGS="--benchmarks=%s --scenarios=offline --config_ver=default --test_mode=PerformanceOnly"' % self.benchmark
+            'make run RUN_ARGS="--benchmarks=%s --scenarios=offline --config_ver=default --test_mode=SubmissionRun"' % self.benchmark
         )
         self.container_platform.with_cuda = True
 
         self.sanity_patterns = sn.all([
-            sn.assert_found(rf'Mode\s*:\s*Performance', self.stdout),
+            sn.assert_found(rf'Scenario\s*:\s*Offline', self.stdout),
+            sn.assert_found(rf'Mode\s*:\s*Submission', self.stdout),
             sn.assert_found(rf'Result\s+is\s*:\s*VALID', self.stdout),
             sn.assert_found(rf'Min\s+duration\s+satisfied\s*:\s*Yes', self.stdout),
             sn.assert_found(rf'Min\s+queries\s+satisfied\s*:\s*Yes', self.stdout),
+            sn.assert_found(rf'Accuracy\s+test\s+PASSED', self.stdout),
 
         ])
 
         self.perf_patterns = {
             'samples_per_second': sn.extractsingle(
-                rf'%s:\s*Samples\s+per\s+second:\s*(?P<samples_per_second>\S+)\s+and\s+Result\s+is\s*:\s*VALID' % self.benchmark,
+                rf'%s:\s+Samples\s+per\s+second:\s*(?P<samples_per_second>\S+)\s+and\s+Result\s+is\s*:\s*VALID' % self.benchmark,
                 self.stdout, 'samples_per_second', float),
+            'accuracy': sn.extractsingle(
+                rf'%s:\s+Accuracy\s+=\s+(?P<accuracy>[.\d]+),\s+Threshold\s+=\s+' % self.benchmark, self.stdout, 'accuracy', float),
         }
 
 
@@ -87,28 +105,137 @@ class MLPerfInference(MLPerfInferenceBase):
             'bert': {
                 'alvis:8xT4': {
                     'samples_per_second': (3456, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA40': {
+                    'samples_per_second': (6572, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM256': {
+                    'samples_per_second': (13465, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM512': {
+                    'samples_per_second': (13465, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM768': {
+                    'samples_per_second': (13465, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100fat': {
+                    'samples_per_second': (13465, -0.05, None, 'samples/s'),
+                    'accuracy': (90.874, -0.01, None, 'accuracy'),
                 },
             },
             'resnet50': {
                 'alvis:8xT4': {
                     'samples_per_second': (46130, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA40': {
+                    'samples_per_second': (74230, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM256': {
+                    'samples_per_second': (150512, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM512': {
+                    'samples_per_second': (150512, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM768': {
+                    'samples_per_second': (150512, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100fat': {
+                    'samples_per_second': (150512, -0.05, None, 'samples/s'),
+                    'accuracy': (76.46, -0.01, None, 'accuracy'),
                 },
             },
             'ssd-resnet34': {
                 'alvis:8xT4': {
                     'samples_per_second': (1086, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA40': {
+                    'samples_per_second': (1877, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM256': {
+                    'samples_per_second': (3947, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM512': {
+                    'samples_per_second': (3947, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM768': {
+                    'samples_per_second': (3947, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100fat': {
+                    'samples_per_second': (3947, -0.05, None, 'samples/s'),
+                    'accuracy': (20.0, -0.01, None, 'accuracy'),
                 },
             },
             'dlrm': {
+                'alvis:8xT4': {
+                    'samples_per_second': (0, -0.05, None, 'samples/s'),
+                    'accuracy': (80.25, -0.01, None, 'accuracy'),
+                },
             },
             'rnnt': {
                 'alvis:8xT4': {
                     'samples_per_second': (10630, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA40': {
+                    'samples_per_second': (24200, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM256': {
+                    'samples_per_second': (41722, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM512': {
+                    'samples_per_second': (41722, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM768': {
+                    'samples_per_second': (41722, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100fat': {
+                    'samples_per_second': (41722, -0.05, None, 'samples/s'),
+                    'accuracy': (100-7.45225, -0.01, None, 'accuracy'),
                 },
             },
             '3d-unet': {
                 'alvis:8xT4': {
                     'samples_per_second': (53, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA40': {
+                    'samples_per_second': (123, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM256': {
+                    'samples_per_second': (183, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM512': {
+                    'samples_per_second': (183, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100_MEM768': {
+                    'samples_per_second': (183, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
+                },
+                'alvis:4xA100fat': {
+                    'samples_per_second': (183, -0.05, None, 'samples/s'),
+                    'accuracy': (0.853, -0.01, None, 'accuracy'),
                 },
             },
         }
