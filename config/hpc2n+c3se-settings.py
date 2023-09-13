d13a34ccb432904c82408ce87853641b301ba5f1
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2023-09-13 08:20:52 +0200

    Fix some typos when setting up vera cluster.

--- config/hpc2n+c3se-settings.py
+++ config/hpc2n+c3se-settings.py
@@ -16,18 +16,18 @@ site_configuration = {
             ],
         },
         {
-            'name': 'skylake',
+            'name': 'vera',
             'descr': 'Vera cluster',
             'hostnames': ['vera'],
             'modules_system': 'lmod',
             'partitions': [
                 {
-                    'name': 'vera',
+                    'name': 'skylake',
                     'descr': 'Skylake nodes',
                     'scheduler': 'slurm',
                     'launcher': 'srun',
                     'access': ['-A C3SE-STAFF', '-C SKYLAKE+MEM96+NOGPU'],
-                    'max_jobs': 100',
+                    'max_jobs': '100',
                     'features': ['cpu', 'intelcpu'],
                     'environs': ['builtin', 'gnu', 'foss', 'foss_2021a', 'foss_2021b', 'foss_2022a', 'intel_2021a', 'intel_2022a'],
                     'container_platforms': [
