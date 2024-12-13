14dc6018fb0ab9b3dbe1147cd05d331b80231ad4
Yunqi Shao <yunqi_shao@yahoo.com>
2024-12-13 16:16:42 +0100

    Simplify structure of numpy check and avoid searching modules

--- checks/apps/python/numpy_check.py
+++ checks/apps/python/numpy_check.py
@@ -8,8 +8,10 @@ import reframe as rfm
 from hpctestlib.python.numpy.numpy_ops import numpy_ops_check
 from reframe.utility import find_modules
 
-class snic_numpy_test(numpy_ops_check):
+@rfm.simple_test
+class numpy_check(numpy_ops_check):
     valid_prog_environs = ['builtin']
+    valid_systems = ['alvis']
     num_tasks_per_node = 1
     use_multithreading = False
     all_ref = {
@@ -60,10 +62,12 @@ class snic_numpy_test(numpy_ops_check):
     maintainers = ['RS', 'TR', 'AS']
 
     @run_after('init')
-    def process_module_info(self):
-        s, e, m = self.module_info
-        self.valid_prog_environs = [e]
-        self.modules = [m]
+    def set_modules(self):
+        module = {
+            'kebnekaise': [],
+            'alvis': ['SciPy-bundle/2023.11-gfbf-2023b'],
+        }
+        self.modules = module.get(self.current_system.name, [])
 
     @run_after('setup')
     def set_num_cpus_per_task(self):
@@ -80,13 +84,3 @@ class snic_numpy_test(numpy_ops_check):
         self.reference = {
             pname: self.all_ref[f'{arch}@{num_cores}c']
         }
-
-@rfm.simple_test
-class c3se_numpy_test(snic_numpy_test):
-    module_info = parameter(find_modules('SciPy-bundle', { r'.*': 'builtin' }))
-    valid_systems = ['alvis']
-
-@rfm.simple_test
-class hpc2n_numpy_test(snic_numpy_test):
-    module_info = parameter(find_modules('SciPy-bundle'))
-    valid_systems = ['kebnekaise']
