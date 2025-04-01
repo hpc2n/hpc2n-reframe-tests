c2ace5e4e934b80bce1c9c9320772eda7715e38b
Yunqi Shao <yunqi.shao@chalmers.se>
2025-04-01 13:21:01 +0200

    tf2_horovod: use +nvgpu as valid_systems

--- checks/apps/tensorflow/tf2_horovod_check.py
+++ checks/apps/tensorflow/tf2_horovod_check.py
@@ -89,11 +89,7 @@ class snic_tensorflow_horovod_check(tensorflow_cnn_check):
     tags |= {'production'}
     maintainers = ['RS', 'TR', 'AS']
     valid_prog_environs = ['builtin']
-
-    valid_systems = ['kebnekaise:%s' % x for x in ['2xK80', '4xK80', '2xV100']]
-    valid_systems += ['alvis:%s' % x for x in [
-        '8xT4', '2xV100', '4xV100', '4xA40', '4xA100_MEM256', '4xA100_MEM512', '4xA100fat'
-    ]]
+    valid_systems = ['+nvgpu']
 
     @run_after('init')
     def set_modules(self):
