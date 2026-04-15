2cc94e93ddfe9cebe8e0fda14b32d756c65ab3fa
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2026-04-15 08:39:30 +0200

    ior_check: add Kebne LargeMem nodes to the test cases

--- checks/system/io/ior_check.py
+++ checks/system/io/ior_check.py
@@ -38,6 +38,10 @@ class IorCheck(rfm.RunOnlyRegressionTest):
                     'num_tasks': 28,
                     'num_tasks_per_node': 28,
                 },
+                'kebnekaise:lm': {
+                    'num_tasks': 72,
+                    'num_tasks_per_node': 72,
+                },
                 'kebnekaise:zen3': {
                     'num_tasks': 128,
                     'num_tasks_per_node': 128,
